2cb081552e7e8feabcda36d4aa2e695063b7d3c5
alexcarrdev <alexcarrdev@gmail.com>
2021-02-09 13:03:37 -0500

    update error handler

--- src/main/java/messaging/MessagingUtils.java
+++ src/main/java/messaging/MessagingUtils.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import service.MessagingController;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
@@ -39,6 +40,9 @@ public class MessagingUtils {
 
    public static void receiveMessages() throws InterruptedException
    {
+
+      CountDownLatch countdownLatch = new CountDownLatch(1);
+
       // consumer that processes a single message received from Service Bus
       Consumer<ServiceBusReceivedMessageContext> messageProcessor = context -> {
          ServiceBusReceivedMessage message = context.getMessage();
@@ -48,9 +52,9 @@ public class MessagingUtils {
       // handles any errors that occur when receiving messages
       Consumer<Throwable> errorHandler = throwable -> {
          System.out.println("Error when receiving messages: " + throwable.getMessage());
-         if (throwable instanceof ServiceBusReceiverException) {
-            ServiceBusReceiverException serviceBusReceiverException = (ServiceBusReceiverException) throwable;
-            System.out.println("Error source: " + serviceBusReceiverException.getErrorSource());
+         if (throwable instanceof Exception) {
+            Exception serviceBusReceiverException = (Exception) throwable;
+            System.out.println("Error source: " + serviceBusReceiverException.getMessage());
          }
          Exception e = (Exception) throwable;
          System.out.println(e.getMessage());
@@ -62,7 +66,7 @@ public class MessagingUtils {
               .processor()
               .queueName(queueName)
               .processMessage(messageProcessor)
-              .processError(errorHandler)
+              .processError(context -> processError(context, countdownLatch))
               .buildProcessorClient();
 
       LOGGER.info("Starting the processor");
@@ -72,5 +76,45 @@ public class MessagingUtils {
       LOGGER.info("Stopping and closing the processor");
       processorClient.close();
    }
+
+   /**
+    * Processes an exception that occurred in the Service Bus Processor.
+    *
+    * @param context Context around the exception that occurred.
+    */
+   private static void processError(ServiceBusErrorContext context, CountDownLatch countdownLatch) {
+      System.out.printf("Error when receiving messages from namespace: '%s'. Entity: '%s'%n",
+              context.getFullyQualifiedNamespace(), context.getEntityPath());
+
+      if (!(context.getException() instanceof ServiceBusException)) {
+         System.out.printf("Non-ServiceBusException occurred: %s%n", context.getException());
+         return;
+      }
+
+      ServiceBusException exception = (ServiceBusException) context.getException();
+      ServiceBusFailureReason reason = exception.getReason();
+
+      if (reason == ServiceBusFailureReason.MESSAGING_ENTITY_DISABLED
+              || reason == ServiceBusFailureReason.MESSAGING_ENTITY_NOT_FOUND
+              || reason == ServiceBusFailureReason.UNAUTHORIZED) {
+         System.out.printf("An unrecoverable error occurred. Stopping processing with reason %s: %s%n",
+                 reason, exception.getMessage());
+
+         countdownLatch.countDown();
+      } else if (reason == ServiceBusFailureReason.MESSAGE_LOCK_LOST) {
+         System.out.printf("Message lock lost for message: %s%n", context.getException());
+      } else if (reason == ServiceBusFailureReason.SERVICE_BUSY) {
+         try {
+            // Choosing an arbitrary amount of time to wait until trying again.
+            TimeUnit.SECONDS.sleep(1);
+         } catch (InterruptedException e) {
+            System.err.println("Unable to sleep for period of time");
+         }
+      } else {
+         System.out.printf("Error source %s, reason %s, message: %s%n", context.getErrorSource(),
+                 reason, context.getException());
+      }
+   }
+}
 
 }
